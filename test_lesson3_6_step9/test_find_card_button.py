c4ad7301d3c368de0e8e3cea1e6a26956789094c
VadimBordiugov <vadim.bordyugov@chainstack.com>
2022-06-29 02:01:50 +0400

    fix asser. this doesn't work if using 'find_element' instead of 'find_elements' test failed before assert,

--- test_lesson3_6_step9/test_find_card_button.py
+++ test_lesson3_6_step9/test_find_card_button.py
@@ -9,7 +9,5 @@ def test_available_product(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     sleep(30)
-    check_available = WebDriverWait(browser, 10)\
-        .until(expected_conditions
-               .element_to_be_clickable((By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')))
+    check_available = browser.find_elements(By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
     assert check_available, f"product is not available, status product is {check_available.text}"
