4d63e538dfaa09f5b078904c4b4cb3b827d218aa
VadimBordiugov <vadim.bordyugov@chainstack.com>
2022-07-04 00:04:53 +0400

    Задание: Группировка тестов: setup

--- test/test_main_page.py
+++ test/test_main_page.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+import pytest
+
 from pages.basket_page import BasketPage
 from pages.locators import BasketPageLocators
 from pages.login_page import LoginPage
@@ -9,19 +11,21 @@ link = "http://selenium1py.pythonanywhere.com/"
 new_link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
 login_page_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    # не забываем передать первым аргументом self
+    def test_guest_can_go_to_login_page(self,browser):
+        page = MainPage(browser, link)       # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                          # открываем страницу
+        page.go_to_login_page()              # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)        # переход на страницу логина (также получаем методы этой страницы)
+        login_page.should_be_login_page()                           # проверки для страницы логина
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)       # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                          # открываем страницу
-    page.go_to_login_page()              # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)        # переход на страницу логина (также получаем методы этой страницы)
-    login_page.should_be_login_page()                           # проверки для страницы логина
 
-
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_login_page(browser):
